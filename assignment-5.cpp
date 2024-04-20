b7aeb6e320256e28ba3ee766e59c4d9629d1cd27
Hamish Hare <22hvh1@queensu.ca>
2024-04-20 23:56:13 +0100

    Main functionality showcase

--- assignment-5.cpp
+++ assignment-5.cpp
@@ -6,101 +6,84 @@
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 // Hamish Hare
 
+#include <memory> // smart pointers
 #include "lepton.h"
 #include "four_momentum.h"
 #include "electron.h"
 #include "muon.h"
 #include "neutrino.h"
 #include "tau.h"
+using std::unique_ptr;
 
 int main()
 {
-  Electron e{1, 1.0, 2.0, 3.0, 4.0};
-  Muon m{1, 10.0, 20.0, 30.0, 40.0, false};
-  Neutrino nt{true, 100.0, 200.0, 300.0, 400.0, false, "TaU"};
-  // e.print_info();
-  // m.print_info();
-  // nT.print_info();
-  FourMomentum fm1{e+m};
-  // FourMomentum fm2{m+nt};
-  // FourMomentum fm3{nt+e};
-  // fm1.print_info();
-  // fm2.print_info();
-  // // fm3.print_info();
-  double p1{dot_product(e, m)};
-  // std::cout<<p1<<std::endl;
-  // double p2{dot_product(m, nt)};
-  // std::cout<<p2<<std::endl;
-  // double p3{dot_product(nt, e)};
-  // std::cout<<p3<<std::endl;
+  // Create the vector of particles
+  unique_ptr<Electron> e1 = std::make_unique<Electron>(-1, 100.1, 200.2, 300.3, 400.4);
+  unique_ptr<Electron> e2 = std::make_unique<Electron>(-1, 123.1, 234.1, 345.6, 456.7);
+  unique_ptr<Muon> m1 = std::make_unique<Muon>(-1, 81.6, 0.0, 0.0, 5.63, false);
+  unique_ptr<Muon> m2 = std::make_unique<Muon>(-1, 984.2, 234.1, 0.0, 0.0, true);
+  unique_ptr<Muon> m3 = std::make_unique<Muon>(-1, 489.21, 0.0, 23.4, 0.0, true);
+  unique_ptr<Muon> m4 = std::make_unique<Muon>(-1, 92.7, 0.0, 0.0, 451.2, false);
+  unique_ptr<Electron> ae1 = std::make_unique<Electron>(1, 712.1, 827.3, 0.0, 0.0);
+  unique_ptr<Muon> am1 = std::make_unique<Muon>(1, 121.1, 0.0, 9.120, 0.0, false);
+  unique_ptr<Neutrino> nm1 = std::make_unique<Neutrino>(false, 11.11, 22.22, 33.33, 44.44, false, "mUon");
+  unique_ptr<Neutrino> ne1 = std::make_unique<Neutrino>(false, 10.01, 20.02, 30.03, 40.04, false, "eLecTRon");
+  unique_ptr<Tau> t1 = std::make_unique<Tau>(-1, 34.2, 56.2, 24.45, 912.3); // FIX: add decay product control
+  unique_ptr<Tau> t2 = std::make_unique<Tau>(-1, 21.3, 89.1, 63.5, 12.45); // FIX: add decay product control
   
-  // Tau t1;
-  // t1.print_info();
-  // t1.decay_hadronically();
-  // t1.decay_leptonically();
-  // t1.decay();
+  vector<unique_ptr<Lepton>> all_particles;
+  all_particles.push_back(std::move(e1)); // 0
+  all_particles.push_back(std::move(e2)); // 1
+  all_particles.push_back(std::move(m1)); // 2
+  all_particles.push_back(std::move(m2)); // 3
+  all_particles.push_back(std::move(m3)); // 4
+  all_particles.push_back(std::move(m4)); // 5
+  all_particles.push_back(std::move(ae1)); // 6
+  all_particles.push_back(std::move(am1)); // 7
+  all_particles.push_back(std::move(nm1)); // 8
+  all_particles.push_back(std::move(ne1)); // 9
+  all_particles.push_back(std::move(t1)); // 10
+  all_particles.push_back(std::move(t2)); // 11
 
-  Tau t2{1, 34.2, 56.2, 24.45, 912.3};
-  t2.print_info();
+  // Print out all the particles
+  for(auto particle{all_particles.begin()};
+      particle<all_particles.end(); ++particle)
+  {
+    (*particle)->print_info();
+  }
 
-  Tau t3{t2};
-  t3.print_info();
-  // // std::cout<<t3.get_decay_type()<<std::endl;
-  // t2.print_info();
+  // Show the functionality
+  std::cout<<std::endl<<std::endl;
+  std::cout<<"==========================="<<std::endl;
 
-  // Tau t4{std::move(t2)};
-  // t4.print_info();
-  // t2.print_info(); // prove moved
+  // .. sum the electrons
+  std::cout<<"Electron sum:"<<std::endl;
+  std::cout<<"e1:"<<std::endl;
+  (*all_particles[0]).print_info();
+  std::cout<<"e2:"<<std::endl;
+  (*all_particles[1]).print_info();
+  FourMomentum fm1{(*all_particles[0])+(*all_particles[1])};
+  fm1.print_info();
 
-  // Tau t5;
-  // t5 = t2;
-  // t5.print_info();
-  // t2.print_info();
+  // .. dot products of four momenta
+  std::cout<<"Dot product:"<<std::endl;
+  std::cout<<"ae1:"<<std::endl;
+  (*all_particles[6]).print_info();
+  std::cout<<"am1:"<<std::endl;
+  (*all_particles[7]).print_info();
+  double dp1{dot_product(*all_particles[6], *all_particles[7])};
+  std::cout<<dp1<<std::endl;
 
-  // Tau t6;
-  // t6 = std::move(t2);
-  // t6.print_info();
-  // t2.print_info(); // prove moved
+  // .. unique ptr for electron & moving
+  std::cout<<"Moving electron:"<<std::endl;
+  unique_ptr<Electron> moved_e = std::make_unique<Electron>(-1, 1.1, 2.2, 3.3, 4.4);
+  std::cout<<"moved electron:"<<std::endl;
+  moved_e->print_info();
+  std::cout<<"new electron:"<<std::endl;
+  Electron new_e = std::move(*moved_e);
+  new_e.print_info();
 
-  // Neutrino nT{true, 111.11, 222.22, 333.33, 444.44, false, "TaU"};
-  // nT.print_info();
-
-  // Electron e1;
-  // e1.print_info();
-
-  // Electron e2{1, 123.1, 234.1, 345.6, 456.7};
-  // e2.print_info();
-
-  // Electron e3{e2};
-  // e3.set_charge(-1);
-  // e3.print_info();
-  // // e2.print_info();
-
-  // Electron e4{std::move(e2)};
-  // e4.print_info();
-  // // e2.print_info(); // Prove e2 has been moved
-
-  // Electron e5;
-  // e5 = e2;
-  // e5.print_info();
-  // e2.print_info();
-
-  // Electron e6;
-  // e6 = std::move(e2);
-  // e6.print_info();
-  // e2.print_info(); // Prove e2 has been moved
-
-  // std::cout<<e2.get_deposited_energies()[0]<<std::endl
-  //          <<e2.get_deposited_energies()[1]<<std::endl
-  //          <<e2.get_deposited_energies()[2]<<std::endl
-  //          <<e2.get_deposited_energies()[3]<<std::endl;
-
-  // vector<double> temp{12.3, 23.4, 34.5, 45.6};
-  // //vector<double> temp{12.3, 23.4, 34.5}; // too few elements
-  // //vector<double> temp{12.3, 23.4, 34.5, 45.6, 56.7}; // too many elements
-  // //vector<double> temp{12.3, 23.4, 34.5, -45.6};  // a negative value
-  // e2.set_deposited_energies(temp);
-  // e2.print_info();
+  // .. shared ptr for tau
 
   return 0;
 }
